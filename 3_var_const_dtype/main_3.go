10c78423e3079a24eda43ad65fd2a003bda6ba26
ricky-kiva <ricky.kiva@gmail.com>
2023-11-21 18:34:59 +0700

    feat(topic-3): separate functions in topic 3

---
+++ 3_var_const_dtype/main_3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// run by `go run .`
+func main() {
+	variableMethod()
+
+	fmt.Print("\n----- VARIABLE POINTER ----- \n\n")
+	variablePointerMethod()
+}
---
+++ 3_var_const_dtype/variable_pointer.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func variablePointerMethod() {
+	var pointer4 uint8 = 4
+	var pointer8 *uint8 = &pointer4 // getting reference of `pointer4`
+	*pointer8 = 1                   // assigning value to the referenced variable of `pointer8` (`pointer4`)
+
+	fmt.Printf("Value of pointer4 now: %d\n", pointer4)
+	fmt.Printf("Value of pointer8 now: %d\n", *pointer8)
+}
