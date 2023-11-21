0107422ea673f3b0a484ad6266458bf8b624e751
ricky-kiva <ricky.kiva@gmail.com>
2023-11-21 13:59:25 +0700

    feat(topic-3): implement pointer variable

--- 3_var_const_dtype/variable.go
+++ 3_var_const_dtype/variable.go
@@ -28,4 +28,13 @@ func main() {
 	decimal = 0.0
 	composite (arrays) = nil
 	*/
+
+	fmt.Print("\n----- VARIABLE POINTER ----- \n\n")
+
+	var pointer4 uint8 = 4
+	var pointer8 *uint8 = &pointer4 // getting reference of `pointer4`
+	*pointer8 = 1                   // assigning value to the referenced variable of `pointer8` (`pointer4`)
+
+	fmt.Printf("Value of pointer4 now: %d\n", pointer4)
+	fmt.Printf("Value of pointer8 now: %d\n", *pointer8)
 }
