d7f5f3829cda03209eeaeaa0746d5916ce040f30
ricky-kiva <ricky.kiva@gmail.com>
2023-11-23 14:31:28 +0700

    feat(topic-6): add closure function impl.

--- 6_function/main_6.go
+++ 6_function/main_6.go
@@ -13,4 +13,25 @@ func main() {
 
 	var average float64 = calculateAverage(4, 8, 12, 16)
 	fmt.Printf("Average: %.2f\n", average)
+
+	// closure function
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+
+		return min, max
+	}
+
+	var numbersForMinMax = []int{2, 3, 4, 5, 6, 10, 100, 1, 85, 23, 56}
+	var min, max = getMinMax(numbersForMinMax)
+	fmt.Printf("min: %d, max: %d\n", min, max)
 }
