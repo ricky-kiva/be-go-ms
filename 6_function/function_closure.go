1bdc66cb67f7decb0989ef91874f73092b6b0dd2
ricky-kiva <ricky.kiva@gmail.com>
2023-11-23 15:15:21 +0700

    refactor(topic-6): move closure impl. to other file

---
+++ 6_function/function_closure.go
@@ -0,0 +1,22 @@
+package main
+
+func getMinMaxClosure() func(n []int) (int, int) {
+
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+
+		return min, max
+	}
+
+	return getMinMax
+}
