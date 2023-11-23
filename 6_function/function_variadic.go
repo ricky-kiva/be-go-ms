a41fdc1f6c1a28d4162639ad3a2b5797af89dd38
ricky-kiva <ricky.kiva@gmail.com>
2023-11-23 14:04:24 +0700

    feat(topic-6): add variadic function impl.

---
+++ 6_function/function_variadic.go
@@ -0,0 +1,11 @@
+package main
+
+func calculateAverage(numbers ...int) float64 {
+	var total int = 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	var avg = float64(total) / float64(len(numbers))
+	return avg
+}
--- 6_function/main_6.go
+++ 6_function/main_6.go
@@ -10,4 +10,7 @@ func main() {
 
 	area, circumference := calculateCircle(5)
 	fmt.Printf("area: %.2f, circumference: %.2f\n", area, circumference)
+
+	var average float64 = calculateAverage(4, 8, 12, 16)
+	fmt.Printf("Average: %.2f\n", average)
 }
