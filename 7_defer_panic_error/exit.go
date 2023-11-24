5a85e1bb84e7f6398cf88aa321a8c19533f279df
ricky-kiva <ricky.kiva@gmail.com>
2023-11-24 21:06:49 +0700

    refactor(topic-7): change defer explanation on print

--- 7_defer_panic_error/exit.go
+++ 7_defer_panic_error/exit.go
@@ -6,7 +6,7 @@ import (
 )
 
 func exitMethod() {
-	defer fmt.Println("This is will run on the end of this function")
+	defer fmt.Println("Due to `exit`, defer won't execute as usual (in the end of function)")
 	os.Exit(1) // exit from `main()` execution
 	fmt.Println("This will run before exit")
 }
