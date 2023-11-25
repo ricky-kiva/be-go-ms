e3a6428f81a07c5d1a4268222272bf580dfcf507
ricky-kiva <ricky.kiva@gmail.com>
2023-11-25 16:36:06 +0700

    feat(topic-9): add context impl. on passing value

---
+++ 9_context_goroutine_channel/context_passing_value.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func contextMethod() {
+	// context transfers values that extends from the parent context
+	ctxParent := context.Background()
+	ctx1 := context.WithValue(ctxParent, "key1", "King")
+	ctx2 := context.WithValue(ctx1, "key2", "Queen")
+	ctx3 := context.WithValue(ctx2, "key3", "Prince")
+	ctx4 := context.WithValue(ctxParent, "key4", "Peoples")
+	ctx5 := context.WithValue(ctx1, "key5", "Council")
+
+	fmt.Println(ctx5.Value("key5"))
+	fmt.Println(ctx5.Value("key4"))
+	fmt.Println(ctx5.Value("key3"))
+	fmt.Println(ctx5.Value("key2"))
+	fmt.Println(ctx5.Value("key1"))
+
+	fmt.Println(ctx3.Value("key2"))
+	fmt.Println(ctx4.Value("key1"))
+}
