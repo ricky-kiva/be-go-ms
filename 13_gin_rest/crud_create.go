3a718a6e5244a81b802c1794d254b101faab863a
ricky-kiva <ricky.kiva@gmail.com>
2023-12-01 03:22:47 +0700

    feat(topic-13): add status field on response

--- 13_gin_rest/crud_create.go
+++ 13_gin_rest/crud_create.go
@@ -11,7 +11,7 @@ func postHandler(c *gin.Context, db *sql.DB) {
 	var newStudent Student
 
 	// `Bind` will parse request body to Struct
-	if c.Bind(&newStudent) == nil { //
+	if c.Bind(&newStudent) == nil {
 		_, err := db.Exec(
 			"insert into students values ($1, $2, $3, $4, $5)",
 			newStudent.Id,
@@ -22,10 +22,18 @@ func postHandler(c *gin.Context, db *sql.DB) {
 		)
 
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "Exec error: " + err.Error()})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"message": "Success create"})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+			return
 		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"message": "Success create",
+		})
+
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Binding Error"})
 	}
