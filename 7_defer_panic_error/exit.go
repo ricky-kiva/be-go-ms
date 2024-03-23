6ba22750fcc01fb3e6e54d932b0dd0aa17ebb103
ricky-kiva <ricky.kiva@gmail.com>
2024-03-23 17:11:53 +0700

    fix(topic-7): fix func. structure

--- 7_defer_panic_error/exit.go
+++ 7_defer_panic_error/exit.go
@@ -7,6 +7,6 @@ import (
 
 func exitMethod() {
 	defer fmt.Println("Due to `exit`, defer won't execute as usual (in the end of function)")
-	os.Exit(1) // exit from `main()` execution
 	fmt.Println("This will run before exit")
+	os.Exit(1) // exit from `main()` execution
 }
